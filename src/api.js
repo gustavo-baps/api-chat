4447b5caa07f66148b5507e06a6d218bb47f9fb7
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-05-22 14:10:49 -0300

    commit 2205 2

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+const token = require("./util/token");
 const salaController = require("./controller/salaController");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
--- src/util/token.js
+++ src/util/token.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const checktoken = async (token, id, key)=>{
-try{
-    const dec = jwt.verify(token, key, err, decoded =>{
-        if (err){
-            return false;
+const checktoken = async (token, id, key) => {
+    try {
+      const dec = jwt.verify(token, key, (err, decoded) => {
+        if (err) {
+          return false;
         }
-        if (decoded){
-            if(decoded.id ==id) return false;
+        if (decoded) {
+          if (decoded.id == id) return false;
         }
-    });
-   
-        return false;
-}catch(e){
-    console.log(e);
-}
+      });
+  
+      return false;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
 };
 
 const setToken = async(id, key)=>{
